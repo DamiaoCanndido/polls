ea56a839fd7412357f7db05ad7f40006077c5185
DamiaoCanndido <tower_gamer@hotmail.com>
2024-02-10 18:54:34 -0300

    create vote

--- src/http/server.ts
+++ src/http/server.ts
@@ -1,11 +1,18 @@
 import fastify from 'fastify';
+import cookie from '@fastify/cookie';
 import { createPoll } from './routes/polls/create';
 import { getPoll } from './routes/polls/get';
+import { voteOnPoll } from './routes/polls/vote-on-poll';
 
 const app = fastify();
 
+app.register(cookie, {
+  secret: 'polls-app-nlw',
+  hook: 'onRequest',
+});
 app.register(getPoll);
 app.register(createPoll);
+app.register(voteOnPoll);
 
 app.listen({ port: 3333 }).then(() => {
   console.log('HTTP server running!');
