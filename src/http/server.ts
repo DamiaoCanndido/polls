31b480e6795c5aed9081fe587bd24574e8d13bec
DamiaoCanndido <tower_gamer@hotmail.com>
2024-02-10 07:44:27 -0300

    get one poll

--- src/http/server.ts
+++ src/http/server.ts
@@ -1,8 +1,10 @@
 import fastify from 'fastify';
 import { createPoll } from './routes/polls/create';
+import { getPoll } from './routes/polls/get';
 
 const app = fastify();
 
+app.register(getPoll);
 app.register(createPoll);
 
 app.listen({ port: 3333 }).then(() => {
