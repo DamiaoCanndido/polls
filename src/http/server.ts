287ad4b514e2ae2f166b2a9aeca4ec8972426bb8
DamiaoCanndido <tower_gamer@hotmail.com>
2024-02-11 18:42:02 -0300

    apply websockets

--- src/http/server.ts
+++ src/http/server.ts
@@ -3,6 +3,8 @@ import cookie from '@fastify/cookie';
 import { createPoll } from './routes/polls/create';
 import { getPoll } from './routes/polls/get';
 import { voteOnPoll } from './routes/polls/vote-on-poll';
+import ws from '@fastify/websocket';
+import { pollResults } from './ws/polls-results';
 
 const app = fastify();
 
@@ -10,9 +12,14 @@ app.register(cookie, {
   secret: 'polls-app-nlw',
   hook: 'onRequest',
 });
+
+app.register(ws);
+
 app.register(getPoll);
 app.register(createPoll);
 app.register(voteOnPoll);
+
+app.register(pollResults);
 
 app.listen({ port: 3333 }).then(() => {
   console.log('HTTP server running!');
